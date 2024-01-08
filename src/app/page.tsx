97356681c3d9bfc93dbc08452414ccb932491886
Hiru1003 <hirumiperera10@gmail.com>
2024-01-08 14:09:43 +0530

    Merge pull request #47 from fossnsbm/feat/development/{39}-{popular-episode-card}
    Feat/development/{39} {popular episode card}

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Hero from "@/components/landing/Hero";
+import Popular_ep from "@/components/popular_episodes/popular_ep";
+import Popular_ep2 from "@/components/popular_episodes/Popular_ep2"; // Fix: Correct the casing of the file path
 import Popular from "@/components/popular_ep/Popular";
+import Logos from "@/components/landing/logos";
+import FOSS_Section from "@/components/landing/foss";
 
 type Props = {};
 
@@ -8,6 +12,10 @@ const Home = (props: Props) => {
   return (
     <div>
       <Hero />
+      <Popular_ep />
+      <Popular_ep2 />
+      <FOSS_Section />
+      <Logos />
     </div>
   );
 };
